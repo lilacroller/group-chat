a467e549a614f9db09a7a2a7e0ea2c8cd8633c41
rishabh <rishabhprsd@proton.me>
2024-02-11 13:31:48 +0530

    Add group request handler

--- message_server.py
+++ message_server.py
@@ -4,6 +4,7 @@ from threading import Thread
 # each cell contains a tuple of form (groupname-ipaddress)
 GroupList= []
 
+context= zmq.Context()
 def GroupList2txt():
 # returns list of group in text version
     result=""
@@ -15,7 +16,6 @@ def GroupList2txt():
 
 def check4usrrequests():
 # stays online and keeps checking for requests from user
-    context= zmq.Context()
     socket= context.socket(zmq.REP)
     socket.bind("tcp://*:1234")
 
@@ -26,9 +26,24 @@ def check4usrrequests():
         socket.send(outmessage.encode())
 
 def check4grprequests():
-    pass
+#    context= zmq.Context()
+    socket= context.socket(zmq.REP)
+    socket.bind("tcp://*:1233")
+
+    while True:
+        inmessage= socket.recv()
+        #group will send the message in form of Name - IP:port
+        inmessage= inmessage.decode().split(' - ')
+        sendername= inmessage[0]
+        senderaddr= inmessage[1]
+        GroupList.append((sendername, senderaddr))
+        print(f"JOIN REQUEST FROM {senderaddr}")
+        outmessage= "SUCCESS"
+        socket.send(outmessage.encode())
 
 if __name__=="__main__":
     GroupList= [("hinge", "12.30.40"), ("dingy", "12.32.12")]
-    thread= Thread(target=check4usrrequests)
-    thread.start()
+    usrthread= Thread(target= check4usrrequests)
+    usrthread.start()
+    grpthread= Thread(target= check4grprequests)
+    grpthread.start()
